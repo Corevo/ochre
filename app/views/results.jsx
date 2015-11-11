f3ec9c3b8bcd35c9297a4dea3294fcfb6361566e
Tomer <gmtomer@gmail.com>
2015-11-11 18:01:26 +0200

    added dummy result

--- app/views/results.jsx
+++ app/views/results.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ResultItem from '../partials/results/result-item.jsx';
 
 export default class Results extends React.Component {
     render () {
         return (
-            <h2>List of items will go here</h2>
+            <ol style={{
+                    paddingLeft: '0px'
+                }}>
+                <ResultItem title="Google" url="https://www.google.com/" date="Sep 4, 1988" summary="Google Inc. is an American multinational technology company specializing in Internet-related services and products." />
+            </ol>
         );
     }
 }
