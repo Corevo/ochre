3fc700895acfb4bc3605245958826a4fb062d309
Tomer <gmtomer@gmail.com>
2016-01-26 11:22:43 +0200

    added search to results view

--- app/views/results.jsx
+++ app/views/results.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ResultItem from '../partials/results/result-item';
 
 export default class Results extends React.Component {
+    static PropTypes = {
+        search: React.PropTypes.func
+    };
     render () {
         return (
             <ol style={{
                     paddingRight: '0px'
                 }}>
                 { this.props.results.map(result => (
-                    <ResultItem key={ result.title } title={result.title} url={ result.url } date={ new Date(result.date).toDateString() } summary={ result.desc ? result.desc.substr(0, 350) : '' } author={result.author} unit={result.unit} tags={result.tags} />
+                    <ResultItem key={ result.title } title={result.title} url={ result.url } date={ new Date(result.date).toDateString() } summary={ result.desc ? result.desc.substr(0, 350) : '' } author={result.author} unit={result.unit} tags={result.tags} search={this.props.search} />
                 ))}
             </ol>
         );
