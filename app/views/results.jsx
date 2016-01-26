d907f98163a9b9e348b37c3230b5e71ccafbc63a
Tomer <gmtomer@gmail.com>
2016-01-26 11:20:43 +0200

    sends tags as prop

--- app/views/results.jsx
+++ app/views/results.jsx
@@ -8,7 +8,7 @@ export default class Results extends React.Component {
                     paddingRight: '0px'
                 }}>
                 { this.props.results.map(result => (
-                    <ResultItem key={ result.title } title={result.title} url={ result.url } date={ new Date(result.date).toDateString() } summary={ result.desc ? result.desc.substr(0, 350) : '' } author={result.author} unit={result.unit} />
+                    <ResultItem key={ result.title } title={result.title} url={ result.url } date={ new Date(result.date).toDateString() } summary={ result.desc ? result.desc.substr(0, 350) : '' } author={result.author} unit={result.unit} tags={result.tags} />
                 ))}
             </ol>
         );
