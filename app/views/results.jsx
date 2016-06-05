52ab0713770d033488131aaa89247e7a4a43eaf6
Tomer <gmtomer@gmail.com>
2016-06-05 11:03:23 +0300

    shows number of results

--- app/views/results.jsx
+++ app/views/results.jsx
@@ -7,13 +7,18 @@ export default class Results extends React.Component {
     };
     render () {
         return (
-            <ol style={{
-                    paddingRight: '0px'
-                }}>
-                { this.props.results.map(result => (
-                    <ResultItem key={ result.title } title={result.title} url={ result.url } date={ new Date(result.date).toDateString() } summary={ result.desc ? result.desc.substr(0, 350) : '' } author={result.author} unit={result.unit} tags={result.tags} search={this.props.search} />
-                ))}
-            </ol>
+            <div>
+                <p style={{
+                    color: "#808080"
+                }}>נמתאו {this.props.results.length} תוצאות</p>
+                <ol style={{
+                        paddingRight: '0px'
+                    }}>
+                    { this.props.results.map(result => (
+                        <ResultItem key={ result.title } title={result.title} url={ result.url } date={ new Date(result.date).toDateString() } summary={ result.desc ? result.desc.substr(0, 350) : '' } author={result.author} unit={result.unit} tags={result.tags} search={this.props.search} />
+                    ))}
+                </ol>
+            </div>
         );
     }
 }
